15bb7a931bb8937a381bcab3985d0f66ce7de707
Iuri Matias <iuri.matias@gmail.com>
2018-03-14 10:48:44 -0400

    experimenting with angular

--- test_app/app/js/my.ts
+++ test_app/app/js/my.ts
@@ -1,7 +1,19 @@
-function greeter(person: string) {
-    return "Hello, " + person;
+//function greeter(person: string) {
+//    return "Hello, " + person;
+//}
+//
+//let user = "Jane User";
+//
+//document.body.innerHTML = greeter(user);
+
+import { Component, Input, Reflect } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'Tour of Heroes';
 }
-
-let user = "Jane User";
-
-document.body.innerHTML = greeter(user);
